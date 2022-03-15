d6780ae03b9e3a45e4fcf71bf57122217f8370b8
William Wong <compulim@users.noreply.github.com>
2022-03-15 16:21:47 +0000

    Add deps

--- src/ui/ToDoItem.js
+++ src/ui/ToDoItem.js
@@ -4,10 +4,10 @@ import './ToDoItem.css';
 const ToDoItem = ({ checked, children, id, onCheck, onDelete, onUncheck }) => {
   const handleCheck = useCallback(
     ({ target: { checked } }) => (checked ? onCheck?.({ id }) : onUncheck?.({ id })),
-    [onCheck, onUncheck]
+    [id, onCheck, onUncheck]
   );
 
-  const handleDelete = useCallback(() => onDelete?.({ id }), [onDelete]);
+  const handleDelete = useCallback(() => onDelete?.({ id }), [id, onDelete]);
 
   return (
     <li className="to-do-item">
